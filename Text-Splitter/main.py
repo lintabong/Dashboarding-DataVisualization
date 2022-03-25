7cb8271156f655d885e5784f79b1a1542104cb25
Lintang Bongkar Girana <96236126+lintabong@users.noreply.github.com>
2022-03-24 23:52:59 -0700

    Update main.py

--- Text-Splitter/main.py
+++ Text-Splitter/main.py
@@ -1,20 +1,22 @@
-mytext  = "ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu"
-numrow = 14
+mytext = input("text: ")
+numrow = input("num : ")
+
+numrow = int(numrow)
 cuttext = ""
 print("text  : ", mytext)
 
-rowprint = int(len(mytext)/numrow)
+rowprint = int(len(mytext) / numrow)
 divider = 0
 
 if len(mytext) >= numrow:
-    for u in range(rowprint+1):
+    for u in range(rowprint + 1):
         divider = divider + numrow
 
-        if divider <= rowprint*numrow:
-            for i in range(divider-numrow, divider):
+        if divider <= rowprint * numrow:
+            for i in range(divider - numrow, divider):
                 cuttext = cuttext + mytext[i]
         else:
-            for i in range(divider-numrow, len(mytext)):
+            for i in range(divider - numrow, len(mytext)):
                 cuttext = cuttext + mytext[i]
-        print("line "+ str(u+1) + ":", cuttext)
+        print("line " + str(u + 1) + ":", cuttext)
         cuttext = ""
